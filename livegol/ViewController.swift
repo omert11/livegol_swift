08f2543ea3cc7726faff9bda1f9bd861d5b54f80
Omer Faruk Yigin <omert1122@gmail.com>
2019-01-24 17:12:24 +0300

    Initial Commit

---
+++ livegol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  livegol
+//
+//  Created by MacUser on 24.01.2019.
+//  Copyright © 2019 MacUser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
